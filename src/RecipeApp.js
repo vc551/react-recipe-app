246dc3fbd673a2bb9bef79c26fa13cf76ff69533
Vikramjit Singh <vikramjit@MacBook-Pro.local>
2018-12-28 16:31:33 -0700

    v2 recipe app

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './RecipeApp.css';
 import Recipe from './Recipe';
+import RecipeList from './RecipeList';
+import Navbar from "./Navbar";
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="RecipeApp">
-        <Recipe 
-          title="Pasta" 
-          ingredients={['flour','water']}
-          instructions="Mix Ingredients"
-          img="pasta.jpg"
-        />
+      <Navbar />
+        <RecipeList />
       </div>
     );
   }
